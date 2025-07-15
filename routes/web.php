a7c6eda7aefbebd3ee775ddf10d8f6bfd44128a8
MeachNha <sokhenq42foryou@gmall.com>
2025-07-15 21:40:57 +0700

    day1

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('index');
+});
+Route::get('/blog', function () {
+    return view('blog');
+})->name('blog');
